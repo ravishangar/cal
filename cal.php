675bdbfbe18ce6b826482aac4146e9c601ec3d82
ravishangar <mirthveen@gmail.com>
2016-12-28 19:26:51 +0530

    new

---
+++ cal.php
@@ -0,0 +1,13 @@
+
+
+<html>
+       <title>PHP - Add two numbers</title>
+       <body>
+              <form action="result.php" method="POST">
+              <input type="text" name="a" value="0" />
+                     <input type="text" name="b" value="0" />
+                     <input type="submit" value="Calculate values"/>
+              </form>
+             
+       </body>
+</html>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>math calculations</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <form action="cal.php" method="POST">
+     <center>
+      
+      <button class ="test" type ="submit" name="plus">plus</button><br>
+      <button class="test">minus</button><br>
+	    <button class="test">mul</button><br>
+	    <button class="test">divide</button>
+     </center>
+		</form>
+</body>
+</html>
